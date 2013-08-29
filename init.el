b2476d50ce94c9d152dc9bb60efb76928be7b2c2
Andrew Keedle <andrew.keedle@btsgroupuk.com>
2013-08-29 10:34:43 +0100

    further nrepl ac el-doc tweaks

--- init.el
+++ init.el
@@ -90,9 +90,22 @@
   (require 'ac-nrepl)
   (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
   (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  (add-hook 'nrepl-interaction-mode-hook
+	    'nrepl-turn-on-eldoc-mode)
 
   (eval-after-load "auto-complete"
     '(add-to-list 'ac-modes 'nrepl-mode))
+
+
+  (defun set-auto-complete-as-completion-at-point-function ()
+    (setq completion-at-point-functions '(auto-complete)))
+
+  (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+  (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+  (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
   (defun live-nrepl-set-print-length ()
     (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
