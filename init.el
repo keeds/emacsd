4979b5d25823e2991e5d3952ba73cce053c53ecd
Andrew Keedle <andrew@numptyheads.com>
2013-11-02 13:09:31 +0000

    merge

--- init.el
+++ init.el
@@ -3,21 +3,25 @@
 
   (require 'package)
   (add-to-list 'package-archives
-	       ;; '("melpa" . "http://melpa.milkbox.net/packages/")
-	       '("marmalade" . "http://marmalade-repo.org/packages/")
+	       '("melpa" . "http://melpa.milkbox.net/packages/")
+	       ;;'("marmalade" . "http://marmalade-repo.org/packages/")
 	       )
   (package-refresh-contents)
   (package-initialize)
 
   (defvar my-packages '(clojure-mode
+ 			;; clojurescript-mode
+			clojure-test-mode
+			cider
 			;; clojure-cheatsheet
 			;; clojure-snippets
 			align-cljlet
 			cljsbuild-mode
+			eldoc
 			paredit
-			nrepl
+			;; nrepl
 			auto-complete
-			ac-nrepl
+			;; ac-nrepl
 			magit
 			zenburn-theme
 			git-gutter
@@ -26,12 +30,14 @@
 			dash
 			rainbow-delimiters
 			highlight-parentheses
-			pg
+			;; pg
 			yaml-mode
 			graphviz-dot-mode
 			;; helm
 			org
-			org-journal))
+			org-journal
+			smartparens
+			htmlize))
 
   (dolist (p my-packages)
     (when (not (package-installed-p p)) (package-install p)))
@@ -69,35 +75,25 @@
 	scroll-conservatively 100000
 	scroll-preserve-screen-position 1)
 
-
   ;; line numbers
   (line-number-mode t)
   (column-number-mode t)
   (size-indication-mode t)
-
   
   ;; undo-tree
   (require 'undo-tree)
   (global-undo-tree-mode 1)
 
-
   ;; git-gutter
   (global-git-gutter-mode t)
-
 
   ;; parens
   (require 'rainbow-delimiters)
   (require 'highlight-parentheses)
 
-  
-  ;; smartparens
-  ;; (require 'smartparens-config)
-
-
   ;; align-cljlet
   (require 'align-cljlet)
   (global-set-key (kbd "C-M-z") 'align-cljlet)
-
 
   ;; auto-complete
   (require 'auto-complete)
@@ -107,18 +103,35 @@
 
   (dolist (mode '(clojure-mode lisp-mode magit-log-edit-mode))
     (add-to-list 'ac-modes mode))
-  
+
+  (require 'smartparens-config)
+  (smartparens-global-strict-mode t)
+  (show-smartparens-global-mode t)
+
+  (define-key sp-keymap (kbd "C-<right>") 'sp-forward-slurp-sexp)
+  (define-key sp-keymap (kbd "C-<left>") 'sp-forward-barf-sexp)
+  (define-key sp-keymap (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+  (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+
+  ;; CIDER
+  (require 'cider)
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+  ;; (add-hook 'cider-repl-mode-hook 'paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'smartparens-mode)
+  (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+  (setq nrepl-hide-special-buffers t)
 
   ;; nrepl
-  (require 'nrepl)
-  (require 'ac-nrepl)
-  (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-  (add-hook 'nrepl-interaction-mode-hook
-	    'nrepl-turn-on-eldoc-mode)
+  ;; (require 'nrepl)
+  ;; (require 'ac-nrepl)
+  ;; (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+  ;; (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  ;; (add-hook 'nrepl-interaction-mode-hook
+  ;; 	    'nrepl-turn-on-eldoc-mode)
 
-  (eval-after-load "auto-complete"
-    '(add-to-list 'ac-modes 'nrepl-mode))
+  ;; (eval-after-load "auto-complete"
+  ;;  '(add-to-list 'ac-modes 'nrepl-mode))
 
 
   (defun set-auto-complete-as-completion-at-point-function ()
@@ -126,19 +139,19 @@
 
   (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-  (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  ;; (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  ;; (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-  (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+  ;; (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
-  (defun live-nrepl-set-print-length ()
-    (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
+  ;; (defun live-nrepl-set-print-length ()
+  ;;  (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
 
-  (add-hook 'nrepl-connected-hook 'live-nrepl-set-print-length)
+  ;; (add-hook 'nrepl-connected-hook 'live-nrepl-set-print-length)
 
 
   ;; postgres
-  (require 'pg)
+  ;; (require 'pg)
 
 
   ;; ibuffer
@@ -154,6 +167,10 @@
 		  (filename . "Projects/mrep"))
 		 ("CMRMapping"
 		  (filename . "Projects/cmrmapping"))
+		 ("Poles"
+		  (filename . "Projects/poles"))
+		 ("Casca"
+		  (filename . "Projects/casca"))
 		 ("Programming" ;; prog stuff not already in MyProject
 		  (or
 		   (mode . emacs-lisp-mode)
@@ -166,20 +183,24 @@
 	      (ibuffer-switch-to-saved-filter-groups "default")))
   
   (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+  ;; clojure-mode
+  (setq clojure-defun-style-default-indent t)
   
   ;; mac osx pound sign
   (define-key global-map (kbd "C-c 3") (lambda () (interactive) (insert "#")))
 
   ;; hook
-  (add-hook 'clojure-mode-hook 'paredit-mode)
+  ;; (add-hook 'clojure-mode-hook 'paredit-mode)
+  (add-hook 'clojure-mode-hook 'smartparens-mode)
   (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'clojure-mode-hook 'highlight-parentheses-mode)
 
-  (add-hook 'nrepl-mode-hook 'paredit-mode)
-  (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
-  (add-hook 'nrepl-mode-hook 'highlight-parentheses-mode)
+  ;; (add-hook 'nrepl-mode-hook 'paredit-mode)
+  ;; (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+  ;; (add-hook 'nrepl-mode-hook 'highlight-parentheses-mode)
   
-  (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+  ;; (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
   )
   
 (add-hook 'after-init-hook 'my-package-complete)
