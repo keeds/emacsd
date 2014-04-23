49218a614ac263773e8f5f8a3f2539c55437b7b2
Andrew Keedle <andrew.keedle@btsgroupuk.com>
2014-04-23 11:35:31 +0100

    safe evaluate for org-babel

--- init.el
+++ init.el
@@ -271,10 +271,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org/keeds.org"
-			    "~/Dropbox/org/bom.org"
-			    "~/Dropbox/org/plan.org"
-			    "~/Dropbox/org/infra.org"))))
+ '(org-agenda-files (quote ("~/Dropbox/org/keeds.org" "~/Dropbox/org/bom.org" "~/Dropbox/org/plan.org" "~/Dropbox/org/infra.org")))
+ '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
